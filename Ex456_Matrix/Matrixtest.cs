04783d963a1b008d15e7b263a417139f67ed3c38
Adelina Ciorap <c.adelina@outlook.com>
2019-07-05 14:04:27 +0300

    456 done

---
+++ Ex456_Matrix/Matrixtest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex456_Matrix
+{
+    class MatrixTest
+    {
+        static void Main(string[] args)
+        {
+            Matrix<int> matrixTesting = new Matrix<int>(10, 10);
+            Matrix<int> matrixTestingTwo = new Matrix<int>(10, 10);
+            Console.WriteLine(matrixTesting.Cols);
+
+            matrixTesting[0, 0] = 200;
+            matrixTestingTwo[0, 0] = 3;
+            Console.WriteLine(matrixTesting + matrixTestingTwo);
+
+            Console.WriteLine(matrixTesting - matrixTestingTwo);
+        }
+    }
+    
+}
---
+++ Exer456.MAtrix/Exer456.MAtrix/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exer456.MAtrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix<int> matrix1 = new Matrix<int>(10, 10);
+            Matrix<int> matrix2 = new Matrix<int>(10, 10);
+            Console.WriteLine(matrix1.Cols);
+
+            matrix1[0, 0] = 187;
+            matrix2[0, 0] = 10;
+            Console.WriteLine(matrix1+ matrix2);
+
+            Console.WriteLine(matrix1 - matrix2);
+        }
+    }
+}
